c399a70aa8a2fd3b8f8ea45141064b85bca22931
MikelIrast <mikelirastorza24@gmail.com>
2022-12-09 16:02:00 +0100

    env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersService } from './users/users.service';
+import { UsersController } from './users/users.controller';
+import {MongooseModule} from "@nestjs/mongoose"
+import {User,UserSchema} from './users/user.schema'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot(process.env.CONNECTION_STRING)
+,MongooseModule.forFeature([{name:User.name,schema:UserSchema}])],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
