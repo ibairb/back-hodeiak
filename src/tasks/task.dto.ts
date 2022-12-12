5a978da239d8860442b23c1efcf45003480cf72c
loki74 <asgardodin74@gmail.com>
2022-12-12 12:19:33 +0100

    cambios en task

--- src/tasks/task.dto.ts
+++ src/tasks/task.dto.ts
@@ -1,14 +1,10 @@
 export class TaskDto {
     taskname:string
+    title: string
     description:string
     user:string
     start: string
     end: string
     color:string
-    title: 'General orientation'
-    // description: '',
-    // allDay: false,
-    // free: false,
-    // color: '#ff9900'
-    
+    // color: '#ff9900'  
 }//class TaskDto
