c6f9b199f212acb2fd1f498d3dfb656a7df9f6c0
loki74 <asgardodin74@gmail.com>
2022-12-16 12:10:32 +0100

    cambios de loe elmentos

--- src/epics/epic.controller.ts
+++ src/epics/epic.controller.ts
@@ -4,43 +4,36 @@ import { EpicService } from "./epic.service";
 import { EpicDTO } from "./epic.dto";
 @Controller('epics')
 export class EpicController {
-    constructor(private epicService: EpicService) {}
+    constructor(private readonly epicService: EpicService) {}
 
     @Get()
-    async getEpics(@Res() res) {
-        const epics = await this.epicService.getEpics();
-        return res.status(HttpStatus.OK).json(epics);
+    async getEpics():Promise<EpicDTO|Object> {
+       return this.epicService.getEpics();
+        
 
-    }
-    @Get(':id')
-    async getEpic(@Res() res, @Param('id') id) {
-        const epic = await this.epicService.getEpic(id);
-        if (!epic) throw new NotFoundException('Epic does not exist!');
-        return res.status(HttpStatus.OK).json(epic);
+    }//getEpics
+    @Get("/:id")
+    async getEpic(@Param('id') id:string):Promise<EpicDTO|Object> {
+         return await this.epicService.getEpic(id);   
+       
     } 
-
-
-    @Post('/create')
+    @Post()
     async createEpic(@Body() body: EpicDTO):Promise<any> {
         
-        return await this.epicService.createEpic(body);
+        await this.epicService.createEpic(body);
+        return {messageCreated:`EPIC CREATED`}
     }
-
    
+    @Delete('/:id')
+    async deleteEpic(@Res() res,@Param("id") id):Promise<any> {
+        await this.epicService.deleteEpic(id);
+        return {messageCreated:`EPIC DELETED`}
+    }   
    
-    @Delete('/delete/:id')
-    async deleteEpic(@Res() res,@Param("id") id) {
-        const epicDeleted = await this.epicService.deleteEpic(id);
-        if (!epicDeleted) throw new NotFoundException('Epic does not exist!');
-        return res.status(HttpStatus.OK).json({
-            message: 'Epic Deleted Successfully',
-            epicDeleted
-        });
-    }
-
-    @Put(':id')
-    updateUser(@Body() body: EpicDTO, @Param("id") id:string):void {
-        this.epicService.updateEpic(id,body)  
+    @Put('/:id')
+    async updateUser(@Body() body: EpicDTO, @Param("id") id:string):Promise<any> {
+        await this.epicService.updateEpic(id,body) ;
+        return {messageCreated:`EPIC UPDATED`} 
     }
 
    
--- src/features/feature.service.ts
+++ src/features/feature.service.ts
@@ -19,29 +19,27 @@ export class FeatureService {
     
     async getFeature(id: string):Promise<Feature|Object> {
         let feature= await this.featureModel.findOne({id:id});
-        if(feature==null) return {error:"Feature does not exit"};
+        if(feature==null) return {error:"FEATURE DOES NOT EXIST"};
         else  return feature;
           
     }//getFeature
 
-    
-   
-
    async createFeature(body: FeatureDto): Promise<any> {
 
         await this.featureModel.collection.insertOne(body);
-
         return { messageCreated: `FEATURE CREATED` }
 
     }//createFeature
-    async updateFeature(id: string, body: any) {
+    async updateFeature(id: string, body: any):Promise<any> {
 
         await this.featureModel.updateOne({ id }, { $set: body });
+        return { messageCreated: `FEATURE UPDATED` }
 
     }//updateFeature
 
-    async deleteFeature(name: string) {
+    async deleteFeature(name: string):Promise<any> {
         await this.featureModel.deleteOne({ name });
+        return { messageCreated: `FEATURE DELETED` }
 
     }//deleteFeature
 
